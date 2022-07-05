01041efaba6c19ef325ff36181658f1792b0d839
Dev7Ayman <89243487+Dev7Ayman@users.noreply.github.com>
2022-07-05 08:50:49 +0300

    Create Update_WAPro.js

---
+++ التحميلات/Update_WAPro.js
@@ -0,0 +1,18 @@
+{
+	"version": "15.1",
+	"download": "https://ymwhatsapp.com/ymwa-business/",
+	"update": [
+		{
+			"languaje": "ar",
+			"changelog": "https://raw.githubusercontent.com/JesusMuentes/YM/main/changelog-B-en.html"
+		},
+{
+			"languaje": "en",
+			"changelog": "https://raw.githubusercontent.com/JesusMuentes/YM/main/changelog-B-en.html"
+		},
+		{
+			"languaje": "es",
+			"changelog": "https://raw.githubusercontent.com/JesusMuentes/YM/main/changelog-B-es.html"
+		}
+	]
+}
