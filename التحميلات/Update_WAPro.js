436796d60f2ffcde66ef7e387fb08ebe93f74f34
Dev7Ayman <89243487+Dev7Ayman@users.noreply.github.com>
2022-07-05 08:52:53 +0300

    Update Update_WAPro.js

--- التحميلات/Update_WAPro.js
+++ التحميلات/Update_WAPro.js
@@ -4,15 +4,15 @@
 	"update": [
 		{
 			"languaje": "ar",
-			"changelog": "https://raw.githubusercontent.com/JesusMuentes/YM/main/changelog-B-en.html"
+			"changelog": "https://raw.githubusercontent.com/Dev7Ayman/YoTS/master/%D8%A7%D9%84%D8%AA%D8%AD%D9%85%D9%8A%D9%84%D8%A7%D8%AA/WAPro_ar.html"
 		},
 {
 			"languaje": "en",
-			"changelog": "https://raw.githubusercontent.com/JesusMuentes/YM/main/changelog-B-en.html"
+			"changelog": "https://raw.githubusercontent.com/Dev7Ayman/YoTS/master/%D8%A7%D9%84%D8%AA%D8%AD%D9%85%D9%8A%D9%84%D8%A7%D8%AA/WAPro_ar.html"
 		},
 		{
 			"languaje": "es",
-			"changelog": "https://raw.githubusercontent.com/JesusMuentes/YM/main/changelog-B-es.html"
+			"changelog": "https://raw.githubusercontent.com/Dev7Ayman/YoTS/master/%D8%A7%D9%84%D8%AA%D8%AD%D9%85%D9%8A%D9%84%D8%A7%D8%AA/WAPro_ar.html"
 		}
 	]
 }
